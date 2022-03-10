977c2c59f4a52081af07863d46c19d5ebdd2183c
Danilazorg <danilazorg7777@gmail.com>
2022-03-10 12:47:21 +0300

    FirstCommit

---
+++ FirstSwift/PageRegistration.swift
@@ -0,0 +1,20 @@
+//
+//  PageRegistration.swift
+//  FirstSwift
+//
+//  Created by Student on 10.03.2022.
+//
+
+import SwiftUI
+
+struct PageRegistration: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PageRegistration_Previews: PreviewProvider {
+    static var previews: some View {
+        PageRegistration()
+    }
+}
---
+++ FirstSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstSwift
+//
+//  Created by Student on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+}
+
